3bdff4134d7bafc3420840b5737555c04430105d
Huong <admin@Apples-MacBook-Pro.net.fpt>
2019-02-16 15:31:09 +0700

    3

---
+++ b.js
@@ -0,0 +1,48 @@
+// console.log(b)
+var a = 'string';
+let b = 12;
+b = 20;
+
+const pi = 3.14
+// pi = 3.12; // false
+let n = true;
+
+// npm i nodemon -save
+
+let arrName = [ 'Teo' , 'Ti', 'Nam' ]
+let person = {
+    name: 'Ti',
+    age: 20
+}
+console.log(person.age)
+// console.log(Array.isArray(person))
+
+// console.log(arrName[1])
+// arrName['key'] = 'Hoa'
+// arrName.push('Hoa')
+
+// console.log(arrName)
+// console.log(typeof arrName)
+
+
+//string
+//number
+//boolean
+//array
+//obj
+//undefined
+//null
+
+// ''
+// 0
+// null
+// false
+// undefined
+// NaN
+
+// console.log(typeof null)
+
+if(NaN){
+    console.log("True")
+}
+else console.log("false")
